08a34b206aaef749b105c9cc754ee3de286a4908
isahanabhat <isahanabhat@gmail.com>
2026-01-21 22:16:12 +0530

    minor changes

--- sudoku/app.py
+++ sudoku/app.py
@@ -12,19 +12,14 @@ def test_app():
     grid = sudoku_obj.remove_values("easy")
     print()
     print(grid)
-    """
+
     input_data = []
     for i in range(9):
         for j in range(9):
-            id_name = "v" + str(i) + str(j)
-            element_value = grid[i][j]
-            element_attributes = {
-                'id' : id_name,
-                'value' : element_value,
-                'readonly': True
-            }
-            input_data.append(element_attributes)
-    """
+            if grid[i][j] == 0:
+                id_name = "v" + str(i) + str(j)
+                input_data.append(id_name)
+    print(input_data)
 
     return render_template('index.html', grid=grid)
 
